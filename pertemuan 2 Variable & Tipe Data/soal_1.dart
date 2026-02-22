0cb152150969c528b63f7a7f9d19773a247196fa
daffaputra09 <daffaputra863@gmail.com>
2026-02-22 16:10:18 +0700

    fix: soal 1

--- pertemuan 2 Variable & Tipe Data/soal_1.dart
+++ pertemuan 2 Variable & Tipe Data/soal_1.dart
@@ -1,14 +1,5 @@
-// void main() {
-//   for (int i = 0; i < 10; i++) {
-//     print('Nama saya adalah Daffa, sekarang berumur ${20 - i}');
-//   }
-// }
-
-
 void main() {
-  String nama = "Daffa";
-  // nama = null; // akan error
-
-  String? alamat;
-  print(alamat);
+  for (int i = 0; i < 10; i++) {
+    print('Nama saya adalah Daffa, sekarang berumur ${20 - i}');
+  }
 }
